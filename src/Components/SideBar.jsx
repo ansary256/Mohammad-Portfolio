74d57cad84b18422a96d288411607ff3fe6cabdb
Mohammad <mhmdansary920@gmail.com>
2026-06-10 23:19:05 +0330

    111

---
+++ src/Components/SideBar.jsx
@@ -0,0 +1,39 @@
+import { Avatar, Grid, Typography, Box, Divider } from "@mui/material";
+
+
+
+import { grey } from "@mui/material/colors";
+
+
+const SideBar = () => {
+    return (
+        <Grid item size={{
+            xs: 0,
+            sm: 0,
+            md: 3,
+            lg: 3,
+            xl: 3
+        }} sx={{ bgcolor: grey[900], height:"100vh"}}>
+            <Box
+                sx={{
+                    textAlign:"center",
+                    justifyContent:"center"
+                }}
+            >
+                <Avatar src={require("../Assets/Imgs/mohammad.jpg")}
+                    sx={{
+                        width: 200, height: 200,
+                        boxShadow: '15px 10px 30px #141414, 5px 4px 10px #141414',
+                        border: '2px solid #607D8B',
+                        margin: "10px auto"
+                    }} variant="circular"></Avatar>
+                <Typography variant="h6" color="whitesmoke"
+                >محمد انصاری</Typography>
+                <Typography variant="caption" color="whitesmoke"
+                >طراح سایت و برنامه نویس</Typography>
+            </Box>
+            <Divider sx={{marginTop:"10px"}} variant="middle"/>
+        </Grid>
+    )
+}
+export default SideBar;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,33 +1,26 @@
 import { ThemeProvider } from '@mui/material/styles';
 import { CacheProvider } from '@emotion/react';
 
-import { Box, Grid } from '@mui/material';
+import { Grid } from '@mui/material';
 
 import { rtlCache } from '../UIComponent/Laws/RTLCache';
 import { theme } from "../UIComponent/Laws/theme";
+import SideBar from '../Components/SideBar';
 
-const MainLayout = () => {
+const MainLayout = ({chidren}) => {
     return (
         <CacheProvider value={rtlCache}>
             <ThemeProvider theme={theme}>
                 <Grid container spacing={2}>
-                    <Grid item size={{
-                        xs: 0,
-                        sm: 0,
-                        md: 3,
-                        lg: 3,
-                        xl: 3
-                    }} sx={{ backgroundColor: "red" }}>
-                        g
-                    </Grid>
+                    <SideBar/>
                     <Grid item size={{
                         xs: 12,
                         sm: 12,
                         md: 9,
                         lg: 9,
                         xl: 9
-                    }} sx={{ backgroundColor: "blue" }}>
-                        h
+                    }}>
+                        {chidren}
                     </Grid>
                 </Grid>
             </ThemeProvider>
